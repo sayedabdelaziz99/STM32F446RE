7795a9a6e0613250eea31d4008fb161fdd5eb555
sayedabdelaziz99 <152058660+sayedabdelaziz99@users.noreply.github.com>
2024-04-03 13:21:47 +0200

    Delete LedDimmer directory

--- LedDimmer/Debug/Drivers/SPI_DRIVER/SPI_driver.d
+++
@@ -1,7 +0,0 @@
-Drivers/SPI_DRIVER/SPI_driver.o: ../Drivers/SPI_DRIVER/SPI_driver.c \
- ../Drivers/SPI_DRIVER/SPI_driver.h \
- E:/MCU1/LedDimmer/Drivers/stm32f446re/STMF446RE.h \
- E:/MCU1/LedDimmer/Drivers/GPIO_DRIVERS/GPIO_driver.h
-../Drivers/SPI_DRIVER/SPI_driver.h:
-E:/MCU1/LedDimmer/Drivers/stm32f446re/STMF446RE.h:
-E:/MCU1/LedDimmer/Drivers/GPIO_DRIVERS/GPIO_driver.h:
--- LedDimmer/Debug/Drivers/Timer_Driver/Timer_Driver.d
+++
@@ -1,6 +0,0 @@
-Drivers/Timer_Driver/Timer_Driver.o: \
- ../Drivers/Timer_Driver/Timer_Driver.c \
- ../Drivers/Timer_Driver/Timer_Driver.h \
- E:/MCU1/LedDimmer/Drivers/stm32f446re/STMF446RE.h
-../Drivers/Timer_Driver/Timer_Driver.h:
-E:/MCU1/LedDimmer/Drivers/stm32f446re/STMF446RE.h:
